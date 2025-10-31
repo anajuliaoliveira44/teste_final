8aa28ad6b2ec765961c190e17352da46fddc7435
Ana Júlia <anajuliaolibeira2007@gmail.com>
2025-10-31 10:45:35 -0300

    projeto finalizado

---
+++ esquema.sql
@@ -0,0 +1,46 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.42 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.11.0.7065
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para teste_final
+CREATE DATABASE IF NOT EXISTS `teste_final` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `teste_final`;
+
+-- Copiando estrutura para tabela teste_final.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela teste_final.users: ~3 rows (aproximadamente)
+INSERT INTO `users` (`id`, `name`, `email`, `created_at`, `password`, `foto`, `nickname`, `updated_at`) VALUES
+	(2, 'usuario01', 'usuario01@usuario.com', '2025-10-23 16:49:03', '$2y$12$fU/szXQIXbvMbcKgHBDp3O0D/m6g03cj/4jTDnqwfYO1TH5ojWJMK', 'imagens/usuarios/usuario_03.jpg', NULL, '2025-10-23 16:49:03'),
+	(3, 'usuario02', 'usuario02@usuario.com', '2025-10-23 17:02:38', '$2y$12$N.ltNNkmin4xi/pJub7NIukWtpd80TLjZh8CRyV/9mwIoWBgpzXXO', 'imagens/usuarios/usuario_02.jpg', NULL, '2025-10-23 17:02:38'),
+	(4, 'usuario03', 'usuario03@usuario.com', '2025-10-23 17:03:37', '$2y$12$omnsqCLzixxK0fHt2kb3Te2lS2pQAMFBEp7d7Rdde0n0Qcs4rKoK2', 'imagens/usuarios/usuario_01.jpg	', NULL, '2025-10-23 17:03:37');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
